60825d4935e83305816ac52ec6fde623dd2e41e9
Tri <hodaitribm224@gmail.com>
2021-12-15 11:07:44 +0700

    foo

--- index.ts
+++ index.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 console.log('local change line 1');
-=======
-console.log('server change line 11');
->>>>>>> b82d53692e7d6d7d5d5ca2195a4c12b4c9f6f7e0
 console.log('server change line 22');
